18a2306be1a378aa3592b77e04cb029a1073448b
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-10 17:21:40 +0900

    App.vue에 footer nav_bar 기능 추가

--- busan-vue/src/App.vue
+++ busan-vue/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <h2>네비바</h2>
+    <nav_bar></nav_bar>
     <router-view></router-view>    
+    <Main_footer></Main_footer>
   </div>
 </template>
 
 <script>
-
+import footer from './components/include/footer.vue'
+import nav_bar from './components/include/nav_bar.vue'
 
 export default {
   name: 'App',
   components: {
-
+    'Main_footer': footer,
+    'nav_bar': nav_bar
   }
 }
 </script>
