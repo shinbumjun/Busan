0d43f59218d0eb06d492d5093f9bdce7be5e79de
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-11 14:49:54 +0900

    vue, django 연결 code 추가

--- busan-vue/src/main.js
+++ busan-vue/src/main.js
@@ -8,3 +8,16 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
+
+/*
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://apidjackets.codewithstein.com'
+
+createApp(App).use(store).use(router, axios).mount('#app')
+*/
