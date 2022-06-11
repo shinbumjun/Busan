45923cdf0b25d28fac76111dc163222d7928cd0b
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-12 00:38:08 +0900

    delete DATABASES code

--- busanpro5/busanpro5/settings.py
+++ busanpro5/busanpro5/settings.py
@@ -107,14 +107,7 @@ pymysql.install_as_MySQLdb()
 # db
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # engine: mysql
-        'NAME' : 'project1', # DB Name
-        'USER' : 'admin', # DB User
-        'PASSWORD' : 'admin123', # Password
-        'HOST': 'database-1.c41a514inqzx.ap-northeast-2.rds.amazonaws.com', # 생성한 데이터베이스 엔드포인트
-        'PORT': '3306', # 데이터베이스 포트
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+
         }
     }
 }
