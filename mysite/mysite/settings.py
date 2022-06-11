0618a6a68dd2811777511ce38a34b44f4e6d60e8
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-11 16:08:53 +0900

    change

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'HOST': 'database-1.c41a514inqzx.ap-northeast-2.rds.amazonaws.com', # 생성한 데이터베이스 엔드포인트
         'PORT': '3306', # 데이터베이스 포트
         'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
 }
