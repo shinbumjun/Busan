f633d9ac559e9c606346fb5ba874a21aa7b6e744
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-11 15:35:26 +0900

    Update main.js

--- busan-vue/src/main.js
+++ busan-vue/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js';
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -9,15 +10,7 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-
-/*
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-
 axios.defaults.baseURL = 'https://apidjackets.codewithstein.com'
 
-createApp(App).use(store).use(router, axios).mount('#app')
-*/
+
+// createApp(App).use(store).use(router, axios).mount('#app')
