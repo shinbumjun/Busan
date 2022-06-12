52dcc568788696f0aee7dd7c739e4a739894e30f
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-12 22:19:56 +0900

    Update main.js

--- busan-vue/src/main.js
+++ busan-vue/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js';
 import axios from 'axios'
+import { store } from './store/store';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
 
