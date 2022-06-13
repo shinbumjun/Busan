3f86e2e4654e91daa9f3944261358ff857372afa
HyoungMinSong <shmin11@naver.com>
2022-06-13 23:13:06 +0900

    업로드파일 지우고 푸쉬

--- busanpro5/busanpro5/settings.py
+++ busanpro5/busanpro5/settings.py
@@ -97,17 +97,7 @@ WSGI_APPLICATION = 'busanpro5.wsgi.application'
 # db
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # engine: mysql
-        'NAME' : 'project1', # DB Name
-        'USER' : 'admin', # DB User
-        'PASSWORD' : 'admin123', # Password
-        'HOST': 'database-1.c41a514inqzx.ap-northeast-2.rds.amazonaws.com', # 생성한 데이터베이스 엔드포인트
-        'PORT': '3306', # 데이터베이스 포트
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
+
 }
 
 # Password validation
