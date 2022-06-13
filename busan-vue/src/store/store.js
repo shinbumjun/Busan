ee738d5345073f3f141d43cd332b037839a8e4a0
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-06-13 19:49:49 +0900

    Update store.js

--- busan-vue/src/store/store.js
+++ busan-vue/src/store/store.js
@@ -1,27 +1,16 @@
 import router from '@/router'
-import vue from 'vue'
-import vuex from 'vuex'
 
-Vue.use(Vuex)
-
-export const store = new Vuex.Store({
-    state: {
-      todoItems: []
-    }
-  });
 
 const userStore = {
     state: {
         user_id: '',
         name: '',
-        email:'',
         token: ''
     },
     mutations: {
         login: function (state, payload) {
             state.user_id = payload.user_id
             state.name = payload.name
-            state.emial = payload.email
             state.token = payload.token
         },
         loginCheck: function (state) {
